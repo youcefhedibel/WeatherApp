ef835163b9c3768d404ba4fedb846b4958815b93
YoucefHedibel <34919049+youcefhedibel@users.noreply.github.com>
2026-04-01 23:51:22 +0100

    craete project files

---
+++ WeatherApp/Sources/App.swift
@@ -0,0 +1,15 @@
+//
+//  App.swift
+//  Manifests
+//
+//  Created by MAC on 01/04/2026.
+//
+import SwiftUI
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Weather app")
+        }
+    }
+}
